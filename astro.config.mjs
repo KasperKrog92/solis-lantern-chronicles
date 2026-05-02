c1a18bd7ad7f1ddb56bbbc75791579110e66e650
Your Name <kkandersen01@gmail.com>
2026-05-02 11:23:34 +0200

    SEO changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://KasperKrog92.github.io',
   base: '/solis-lantern-chronicles',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   vite: {
     optimizeDeps: {
       include: ['@3d-dice/dice-box-threejs'],
